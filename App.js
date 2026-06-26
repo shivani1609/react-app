cb71a4cd96b8efd34592938413b485d5d3880fb2
Shivani Yadav <shivaniyadav@Shivanis-MacBook-Air.local>
2026-06-26 18:01:01 +0530

    Initial react base code- Ep:1

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("h1", 
+    {
+        "id": "heading",
+        "className": "head",
+        "name": "shivani"
+    }, 
+    "Hello World..!!"
+);
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
