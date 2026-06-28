3a4a46eee8634408df8b4d3bf1351de8b1a4278d
Shivani Yadav <shivaniyadav@Shivanis-MacBook-Air.local>
2026-06-28 16:08:59 +0530

    functional component & component composition

--- App.js
+++ App.js
@@ -2,7 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // JSX => Babel Transpiler => React.createElement() => React Element- JS Object => React DOM; HTMLRenderer => HTML Element => Browser DOM
-const jsxHeading = <h1 id="title">Hello React JSX</h1>;
+// React Element is a JS Object which is used to create HTML Element in the Browser DOM using ReactDOM.render() method.
+const Title = () => (
+    <h1 id="title" className="heading">
+        Hello React JSX
+    </h1>
+);
+
+// React Functional Component is a JS Function which returns React Element(JS Object) and it is used to create HTML Element in the Browser DOM using ReactDOM.render() method.
+const HeadingComponent = () => (
+    <div>
+         {/* Component Composition - A component can be used inside another component. */}
+        <Title />
+        <h1>Heading Component</h1>
+        <p>This is a paragraph inside the heading component.</p>
+    </div>
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
