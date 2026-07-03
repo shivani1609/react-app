fe34f0964860d79b4236b950e64d1145e48c1a31
Shivani Yadav <shivaniyadav@Shivanis-MacBook-Air.local>
2026-07-03 19:33:14 +0530

    login/logut button added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,27 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
 
-const Header = () => (
-    <div className="header">
-        <div className="logo-container">
-            <img className="logo" src={LOGO_URL} alt="Logo" />
+const Header = () => {
+    const [loginBtn, setLoginBtn] = useState("Login");
+
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={LOGO_URL} alt="Logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                    <button className="login" onClick={() => loginBtn === "Login" ? setLoginBtn("Logout") : setLoginBtn("Login")}>
+                        {loginBtn}
+                    </button>
+                </ul>
+            </div>
         </div>
-        <div className="nav-items">
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Cart</li>
-            </ul>
-        </div>
-    </div>
-);
+    );
+}
 
 export default Header;
