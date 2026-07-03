c62e0686166a996b6bd197489f01ce354d1e99a5
Shivani Yadav <shivaniyadav@Shivanis-MacBook-Air.local>
2026-07-03 14:17:29 +0530

    Restaurant API integration & Mock data removed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,32 @@
 import RestaurantCard from "./RestaurantCard";
-import restaurantsList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () =>  {
-    const [listOfRestaurants, setListOfRestaurants] = useState(restaurantsList);
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.97530&lng=77.59100&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+
+        setListOfRestaurants(json?.data?.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    }
+
+    if(listOfRestaurants.length === 0) {
+        return <Shimmer />
+    }
+
     return (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={() => {
-                    const filteredList = restaurantsList.filter((restaurant) => restaurant.info.avgRating > 4);
+                    const filteredList = listOfRestaurants.filter((restaurant) => restaurant.info.avgRating > 4);
                     setListOfRestaurants(filteredList); 
-                    console.log("Original List:", restaurantsList);
+                    console.log("Original List:", listOfRestaurants);
                     console.log("Filtered List:", filteredList);
                 }}>
                     Top Rated Restaurants
@@ -18,7 +34,7 @@ const Body = () =>  {
             </div>
             <div className="restaurant-list">
                 {
-                    listOfRestaurants.map((restaurant) => (
+                    listOfRestaurants?.map((restaurant) => (
                         <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                     ))
                 }
