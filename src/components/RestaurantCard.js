a4a92bb862a402cccaf6c05d5bbb4be8e3e004a3
Shivani Yadav <shivaniyadav@Shivanis-MacBook-Air.local>
2026-07-03 22:26:32 +0530

    costForTwo added in restaurant card

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,7 +2,7 @@ import { CDN_URL } from "../utils/constant";
 
 const RestaurantCard = (props) => {
     const { resData } = props;
-    const { name, cuisines, avgRating, sla, cloudinaryImageId } = resData.info;
+    const { name, cuisines, avgRating, sla, cloudinaryImageId, costForTwo } = resData.info;
 
     return (
         <div className="restaurant-card" style={{ backgroundColor: "#f0f0f0", padding: "10px", margin: "10px", borderRadius: "5px" }}>
@@ -10,6 +10,7 @@ const RestaurantCard = (props) => {
             <h2 className="restaurant-name">{name}</h2>
             <p className="restaurant-cuisine">{cuisines.join(", ")}</p>
             <p className="restaurant-rating">{avgRating} ⭐</p>
+            <p className="restaurant-price"> {costForTwo}</p>
             <p className="restaurant-delivery-time">{sla.deliveryTime} mins </p>
         </div>
     )
